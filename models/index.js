b76996e36bcc5692c04e046409e50b4ce750e13c
aaronespinoza <aarone@berkeley.edu>
2022-02-02 21:19:16 -0800

    deleted an s in products model index

--- models/index.js
+++ models/index.js
@@ -6,25 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "category_id"
-})
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "category_id"
-})
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id"
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Products, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "tags_id"
-})
+});
 
 module.exports = {
   Product,
